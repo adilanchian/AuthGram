f5a6f76bda6388b765907129048f37710637ac56
Alec Dilanchian <alec.dilanchian@gmail.com>
2018-06-03 12:25:32 -0500

    Cleaning up old solution

--- AuthGram-Server/server.js
+++ AuthGram-Server/server.js
@@ -1,16 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const server = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const baseApiPath = '/api/v1';
 
-// Database //
-const db = require('./database/db');
+server.use(bodyParser.raw());
 
 // Routes //
 server.get('/', (req, res) => {
-  console.log('Root Route Hit.');
-  res.send('Welcome to AuthGram!');
+	console.log('Root Route Hit.');
+	res.send('Welcome to AuthGram!');
+});
+
+server.get(`${baseApiPath}/images`, async (req, res) => {
+	console.log('Fetching all images...');
+
+	res.send({ images: images });
+});
+
+server.post(`${baseApiPath}/image/upload`, async (req, res) => {
+	console.log('Posting all images...');
+
+	// Write body stream to file //
+	let filename = req.headers['x-filename'];
+	res.sendStatus(200);
 });
 
 server.listen(8000, () => {
-  console.log('AuthGram server port 8000 listening!');
+	console.log('AuthGram server port 8000 listening!');
 });
